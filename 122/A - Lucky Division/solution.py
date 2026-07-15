e9f12ffebc985e24681fd9dee5f15942cb4d57c2
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-07-16 00:08:15 +0530

    Add Lucky Division [A] from Codeforces

---
+++ 122/A - Lucky Division/solution.py
@@ -0,0 +1,8 @@
+lucky = [4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777]
+n = int(input())
+for x in lucky:
+    if n%x == 0:
+        print("YES")
+        break
+else:
+    print("NO")
